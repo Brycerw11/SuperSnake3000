5b1fb1d11b025f9ec59965d21b995974c7c5ac02
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-03-12 01:55:02 -0700

    feat: update leaderboards with local storage values

--- scripts/mainmenu/LeaderboardHandler.js
+++ scripts/mainmenu/LeaderboardHandler.js
@@ -6,33 +6,86 @@ $(function (){
     var mediumLeaderboardValue;
     var hardLeaderboardValue;
 
-    createTestLeaderboardData()
+    // createTestLeaderboardData()
+    // clearLeaderboardData();
 
     try{
-        easyLeaderboardValue = localStorage.getItem("SuperSnake3000EasyLeaderboard")
-        mediumLeaderboardValue = localStorage.getItem("SuperSnake3000MediumLeaderboard")
-        hardLeaderboardValue = localStorage.getItem("SuperSnake3000HardLeaderboard")
+        localStorage.setItem("SuperSnakeTest-1234567890", 1234567890);
+        localStorage.removeItem("SuperSnakeTest-1234567890");
     }
     catch (error){
-        $(".leaderboard-values p").text("This browser does not support LocalStorage OR LocalStorage is disable; the leaderboard is unavailable as a result.")
+        $(".leaderboard-values p").text("This browser does not support LocalStorage OR LocalStorage is disabled; the leaderboard is unavailable as a result.")
         return; //can't make leaderboard if can't retrieve leaderboard data
     }
 
-    easyLeaderboardArray = easyLeaderboardValue.split(" | ")
-    mediumLeaderboardArray = easyLeaderboardValue.split(" | ")
-    hardLeaderboardArray = easyLeaderboardValue.split(" | ")   
+    //must support local storage, so retrive the data
+    easyLeaderboardValue = localStorage.getItem("SuperSnake3000EasyLeaderboard")
+    mediumLeaderboardValue = localStorage.getItem("SuperSnake3000MediumLeaderboard")
+    hardLeaderboardValue = localStorage.getItem("SuperSnake3000HardLeaderboard")
+
+    //The local storage data structure for the Leaderboards:
+    // "firstPlacesName, firstPlaceScore | secondPlacesName, secondPlaceScore | ... and so on "
+
+
+    //Add easy leaderboard's values
+    var position = 1;
+    if (easyLeaderboardValue != null){
+        $("#easy-leaderboard .leaderboard-values").html("") //clear out current values / placeholder
+
+        easyLeaderboardValue.split(" | ").forEach((element) => {
+            var currentData = element.split(", "); //current data will be an array set up like: [playerName, playerScore]
+    
+            let playerNameLabel = `<div>${position}. <span class="player-name">${currentData[0]}</span></div>`
+            let scoreLabel = `<div>${currentData[1]}</div>`
+            $("#easy-leaderboard .leaderboard-values").append(`<li> ${playerNameLabel} ${scoreLabel} </li>`)
+    
+            position++;
+        });
+    }
+
+    //add medium leaderboard's values
+    position = 1;
+    if (mediumLeaderboardValue != null){
+        $("#medium-leaderboard .leaderboard-values").html("") //clear out current values / placeholder
+
+        mediumLeaderboardValue.split(" | ").forEach((element) => {
+            var currentData = element.split(", "); //current data will be an array set up like: [playerName, playerScore]
+    
+            let playerNameLabel = `<div>${position}. <span class="player-name">${currentData[0]}</span></div>`
+            let scoreLabel = `<div>${currentData[1]}</div>`
+            $("#medium-leaderboard .leaderboard-values").append(`<li> ${playerNameLabel} ${scoreLabel} </li>`)
+    
+            position++;
+        });
+    }
+
+    //add hard leaderboard's values
+    position = 1;
+    if (hardLeaderboardValue != null){
+        $("#hard-leaderboard .leaderboard-values").html("") //clear out current values / placeholder
+
+        hardLeaderboardValue.split(" | ").forEach((element) => {
+            var currentData = element.split(", "); //current data will be an array set up like: [playerName, playerScore]
+    
+            let playerNameLabel = `<div>${position}. <span class="player-name">${currentData[0]}</span></div>`
+            let scoreLabel = `<div>${currentData[1]}</div>`
+            $("#hard-leaderboard .leaderboard-values").append(`<li> ${playerNameLabel} ${scoreLabel} </li>`)
+    
+            position++;
+        });
+    }
 
 
     function createTestLeaderboardData(){
-        localStorage.setItem("SuperSnake3000EasyLeaderboard", `Player Name 1, 20 | Player Name 2, 25`)
-        localStorage.setItem("SuperSnake3000MediumLeaderboard", `Player Name 1, 20 | Player Name 2, 25`)
-        localStorage.setItem("SuperSnake3000HardLeaderboard", `Player Name 1, 20 | Player Name 2, 25`)
+        localStorage.setItem("SuperSnake3000EasyLeaderboard", `Player Name 1, 20.3 | Player Name 2, 25.3`)
+        localStorage.setItem("SuperSnake3000MediumLeaderboard", `Player Name 1, 30.7 | Player Name 2, 35.5`)
+        localStorage.setItem("SuperSnake3000HardLeaderboard", `Player Name 1, 40.1 | Player Name 2, 45.9`)
     }
     
     function clearLeaderboardData(){
-        localStorage.setItem("SuperSnake3000EasyLeaderboard", null)
-        localStorage.setItem("SuperSnake3000MediumLeaderboard", null)
-        localStorage.setItem("SuperSnake3000HardLeaderboard", null)
+        localStorage.removeItem("SuperSnake3000EasyLeaderboard")
+        localStorage.removeItem("SuperSnake3000MediumLeaderboard")
+        localStorage.removeItem("SuperSnake3000HardLeaderboard")
     }
 
 })
