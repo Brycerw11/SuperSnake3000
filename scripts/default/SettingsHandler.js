e5258b1deea9b216b8c16f2b8fb6dd6e201784c5
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-03-22 00:48:59 -0700

    feat: finish options page for survival

--- scripts/default/SettingsHandler.js
+++ scripts/default/SettingsHandler.js
@@ -19,17 +19,117 @@ $(function(){
             setTimeout(function(){window.location.href = "../index.html";}, 3000)
     }
 
+    const easySettings = {
+        gameSize: 72,
+        snakeStartLength: 3,
+        snakeGrowthInterval: 7.5,
+        snakeMoveSpeed: 1,
+        startEnemySpawnInterval: 8,
+        minEnemySpawnInterval: 5,
+        borderoverflow: "Off",
+    }
+    const mediumSettings = {
+        gameSize: 64,
+        snakeStartLength: 3,
+        snakeGrowthInterval: 6,
+        snakeMoveSpeed: 1,
+        startEnemySpawnInterval: 6,
+        minEnemySpawnInterval: 3.5,
+        borderoverflow: "Off",
+    }
+    const hardSettings = {
+        gameSize: 48,
+        snakeStartLength: 5,
+        snakeGrowthInterval: 4.5,
+        snakeMoveSpeed: 2,
+        startEnemySpawnInterval: 5,
+        minEnemySpawnInterval: 2,
+        borderoverflow: "Off",
+    }
+
     function createSurvivalOptions(){
         primaryOptionsSectionHTML = `<div class="options-section">
             <div class="spliting-div">
-            <label for="dificulty-selector">Difficulty:</label>
-                <select id="dificulty-selector" name="dificulty-selector-name">
-                    <option value="easy">Easy</option>
-                    <option value="medium" selected>Medium</option>
-                    <option value="hard">Hard</option>
+            <label for="difficulty-selector">Difficulty:</label>
+                <select id="difficulty-selector" name="difficulty-selector-name">
+                    <option value="Easy">Easy</option>
+                    <option value="Medium" selected>Medium</option>
+                    <option value="Hard">Hard</option>
                 </select>
             </div>
         </div>`
         $("#options").append(`<h3 class="options-section-header">Primary Settings</h3> ${primaryOptionsSectionHTML}`)
+        
+        // Add all the settings that can change depending on the difficult and their default (medium) states
+        $("#options").append(`<h4 id="current-difficulty-header">Current Difficulty Choice: Medium</h4>`)
+        $("#options").append(`<div class="centering-div"> <ul id="current-difficulty-effects"></ul> </div>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Game Size:</div> <div>64x64</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Snake Start Length:</div> <div>3 units</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Snake Growth Interval:</div> <div>every 6s</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Snake Move Speed:</div> <div>1 u/s</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Starting Enemy Spawn Interval:</div> <div>every 7s</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Min Enemy Spawn Interval:</div> <div>every 3.5s</div></li>`)
+        $("#current-difficulty-effects").append(`<li><div class="bold-line">Boder Overflow:</div> <div>Off</div></li>`)
+
+        $("#difficulty-selector").on("change", function(){
+            let currentDifficulty = document.getElementById("difficulty-selector").value
+
+            var currentSurvivalSettings;
+            switch(currentDifficulty){
+                case "Easy":
+                    currentSurvivalSettings = easySettings;
+                    break;
+                case "Medium":
+                    currentSurvivalSettings = mediumSettings;
+                    break;
+                case "Hard":
+                    currentSurvivalSettings = hardSettings;
+                    break;
+            }
+
+            var targetSettingValue = `${currentSurvivalSettings.gameSize}x${currentSurvivalSettings.gameSize}`
+            for(let i=0; i < 6; i++){
+                $("#current-difficulty-effects li").eq(i).find("div").eq(1).text(targetSettingValue);
+
+                switch(true){ // Update target setting text
+                    case i == 0:
+                        targetSettingValue = `${currentSurvivalSettings.snakeStartLength} units`;
+                        break;
+                    case i == 1:
+                        targetSettingValue = `every ${currentSurvivalSettings.snakeGrowthInterval}s`;
+                        break;
+                    case i == 2:
+                        targetSettingValue = `${currentSurvivalSettings.snakeMoveSpeed} u/s`;
+                        break;
+                    case i == 3:
+                        targetSettingValue = `every ${currentSurvivalSettings.startEnemySpawnInterval}s`;
+                        break;
+                    case i == 4:
+                        targetSettingValue = `every ${currentSurvivalSettings.minEnemySpawnInterval}s`;
+                        break;
+                }
+            }
+
+            $("#current-difficulty-header").text("Current Difficulty Choice: " + currentDifficulty)
+        })
+
+        $("#how-to-play-text").text("Use W, A, S, and D or the Arrow Keys to move. Your snake, the green snake, will grow automatically and the red snakes are the enemies which appear periodically. Hitting the border, enemies, or your own snake will cause the game to end.")
     }
+
+
+
+    //create play game button
+    $("#main-menu-button").on("click", function(){
+        window.location.href = "../index.html";
+    })
+    $("#play-button").on("click", function(){
+        switch(gameMode){
+            case "Survival":
+                localStorage.setItem("SuperSnake3000-Settings-SurvivalDifficulty", document.getElementById("difficulty-selector").value)
+                break
+
+        }
+
+        window.location.href = "game.html";
+    })
 })
