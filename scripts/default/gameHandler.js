6d074aa27cd7046d8a2ce57042db34bcd9171889
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-04-17 17:58:16 -0700

    feat: Finish Up basic Snake Game
    Add Score
    Add How to play
    Wrapped up loose ends

--- scripts/default/gameHandler.js
+++ scripts/default/gameHandler.js
@@ -1,8 +1,10 @@
 // Wait for document to be ready before execution
 $(function(){
 
+    // Hide scores before game has started
+    $(".start-hidden").slideUp();
+
     var gameHeight;
-    // var gameState = [];
     var gameInterval;
     
     
@@ -13,7 +15,9 @@ $(function(){
     var snakeState = [[25, 25]];
     var applePos = [0, 0];
     var survivalTime = 0;
+    var snakeLength = 1;
 
+    // Handle Button Presses
     $("body").on("keydown", function(event){
         switch(event.code){
             // Up Direction
@@ -63,12 +67,37 @@ $(function(){
         }
     })
 
-    function endGame(){
+    function endGame(causeOfDeath="the border"){
         clearInterval(gameInterval);
 
-        $("#game-grid").html(`<div class="centering-div"> <h2>You Lose!</h2> </div>`)
-        $("#game-grid").css("background-color", "black");
-        $("#game-grid h2").css("color", "white")
+        // Get final Time
+        let timeText = (survivalTime /10).toString();
+        // If the time is a whole number, add the decimal point for clarity
+        if(timeText[timeText.length -2] != "."){
+            timeText = timeText + ".0"
+        }
+        timeText = timeText + "s"
+
+        let formattedSnakeLength;
+
+        // Get final snake length with proper units
+        if(snakeLength == 1){
+            formattedSnakeLength = `${snakeLength} unit`;
+        }
+        else{
+            formattedSnakeLength = `${snakeLength} units`
+        }
+
+        let finalScoresHTML = `<h2>Your Final Scores:</h2> <h3>Final Snake Length: ${formattedSnakeLength}</h3> <h3>Total Survival Time: ${timeText}</h3>`;
+        let gameGridHTML = `<div style="text-align:center;"> <h1 style="margin-bottom:0.25rem;">You Lose!</h1> <h4 style="margin-top:0.25rem;">You died to ${causeOfDeath}</h4> <hr> ${finalScoresHTML} <br><hr><br> <p>Press the "Start Game" button above to restart the game.</p> </div>`
+
+        $("#game-grid").html(gameGridHTML)
+        $("#game-grid").css({"background-color": "black", "color": "white"});
+        $("#game-grid h3").css("margin", "0.2rem")
+        $("#game-grid hr").css({"margin": "auto", "width": "50%"})
+
+        $("#score-section").slideUp(100);
+        $("#game-control-buttons-container").slideDown(100);
     }
 
     function drawGameState(){
@@ -82,12 +111,29 @@ $(function(){
         for (segment of snakeState){
             $(`#game-grid #row-${segment[0]} #col-${segment[1]}`).css("background-color", "green");
         }
+
+        // Update the score headers
+        if(snakeLength == 1){
+            $("#length-score-header").text(`Snake Length: ${snakeLength} unit`)
+        }
+        else{
+            $("#length-score-header").text(`Snake Length: ${snakeLength} units`)
+        }
+
+
+        let timeText = (survivalTime /10).toString();
+        // If the time is a whole number, add the decimal point for clarity
+        if(timeText[timeText.length -2] != "."){
+            timeText = timeText + ".0"
+        }
+        $("#time-score-header").text(`Survival Time: ${timeText}s`)
     }
 
     // gameUpdate is called every fifth of a second (200ms)
     function gameUpdate(){
 
-        // The snake pos is stored as: [y, x]
+        // Find and store the position that the snake is trying to move to
+        //      The snake pos is stored as: [y, x]
         switch(snakeDirection){
             case "Up":
                 snakeHeadPos[0]--;
@@ -112,7 +158,7 @@ $(function(){
         if(snakeHeadPos[0] >= 50 || snakeHeadPos[1] >= 50 || snakeHeadPos[0] < 0 || snakeHeadPos[1] < 0){
             // If player hits border
             console.log(`You hit border: ${snakeHeadPos}`);
-            endGame();
+            endGame("the border");
             return;
         }
 
@@ -122,12 +168,11 @@ $(function(){
                 // If player hits snake
                 console.log(`You hit yourself: \n Head Pos:${snakeHeadPos}\n Segment: ${segment}`);
                 console.table(snakeState)
-                endGame();
+                endGame("yourself");
                 return;
             }
         } 
         
-        // console.log(`Collision check:\nTime: ${survivalTime}\nApple Pos:${applePos}\nSnake Head Pos: ${snakeHeadPos}`)
         let hasHitApple = false;
         // Check to see if snake hit apple
         if(snakeHeadPos[0] == applePos[0] && snakeHeadPos[1] == applePos[1]){
@@ -148,7 +193,9 @@ $(function(){
                     break;
                 }
             }
-            console.log(`Updated Apple Pos: ${applePos}`)
+
+            // Increase the snake length score
+            snakeLength++;
         }
 
         // add new snake segment into snake state (at end of array)
@@ -157,23 +204,22 @@ $(function(){
         // snake grows by not removing oldest snake segment after eating an apple
         if(!hasHitApple){
             snakeState.shift();// remove oldest snake segment
-
-            // console.log(`Removed: ${snakeState.shift()}`) 
         }
 
+        survivalTime += 2; // survival time is a fixed point number so this adds .2
         drawGameState();
-        survivalTime += 0.2;
     }
 
     $("#start-button").on("click", function(){
 
         $("#game-control-buttons-container").slideUp(150);
 
-        //set the sides of the game-grid to 75% of the window width or height depending on which is smaller.
+        // Set the sides of the game-grid to 75% of the window width or height depending on which is smaller.
+        // The game grid is a square so the height is also the width
         gameHeight = 0.75 * Math.min(window.innerWidth, window.innerHeight);
-        console.log(`Win Width is ${window.innerWidth}px`)
-        console.log(`Win Height is ${window.innerHeight}px`)
-        console.log(`Game Grid Size is ${gameHeight}x${gameHeight}px`)
+
+        // Ensure the score section has the same width as the game-grid
+        $("#score-section").css("width", gameHeight)
 
         //Animate the game-grid to the proper size and content
         $("#game-grid").fadeOut(100) //animate old content away
@@ -223,14 +269,28 @@ $(function(){
             snakeDirection = "None";
             lastMove = "None";
 
+            // Set starting positions for things
             snakeHeadPos = [25, 25];
             snakeState = [[25, 25]];
             applePos = [Math.floor(Math.random() * 50), Math.floor(Math.random() * 50)];
 
+            snakeLength = 1;
             survivalTime = 0;
+
+            $("#score-section").slideDown();
 
             gameInterval = setInterval(gameUpdate, 200);
         }, 700)
+    })
+
+    $("#htp-button").on("click", function(){
+        let htpIntroText = "Survive as long as possible without hitting the border or yourself while eating as many apples as possible to grow longer to try to take up all 250 spaces of the 50x50 grid!"
+        let htpLayoutText = "This version of snake takes place in 50 by 50 grid with a total of 250 spaces. You are a green snake who starts with a length of 1 unit who gains an additional unit of length by eating a red apple. Your snake always starts in the middle of the grid, but the apple's position is randomized."
+        let htpControlsText = "You can use either W, A, S, and D to move up, left, down, and right respectively or the arrow keys the move in the same direction the key faces. "
+        
+        $("#game-grid").html(`<div style="margin: 0 5%;"> <p style="text-indent:0rem;">${htpIntroText}</p> <h3>Layout</h3> <p>${htpLayoutText}</p> <h3>Controls</h3> <p>${htpControlsText}</p> </div>`)
+        $("#game-grid p").css("text-indent", "1.5rem")
+        $("#game-grid p").eq(0).css("text-indent", "0")
     })
 
 })
